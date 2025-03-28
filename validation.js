e573749863695660bddfe83f11c57764d50d4d09
okwirrry <fredrickokwiri30@gmail.com>
2025-03-28 16:06:35 +0300

    Update validation.js

--- validation.js
+++ validation.js
@@ -87,7 +87,7 @@ else{
         emailerror.textContent = "";
         telerror.textContent = "";
         textareaerror.textContent = "";
-    }, 50000);w
+    }, 100000);w
     });
 
 document.getElementById('modal_form').addEventListener('submit',function valid(e){
@@ -126,7 +126,7 @@ if(services.selectedIndex===0){
         emailerror.textContent = "";
         phoneerror.textContent = "";
         serviceserror.textContent = "";
-    }, 50000);
+    }, 10000);
    
 if(isvalid){
     this.submit();
